5c1f1859134ab70a207d04efe04d9607506ab575
Žilvinas Mockus <36519492+zilvmock@users.noreply.github.com>
2024-12-01 20:30:26 +0200

    Initial Code (#1)

---
+++ Tests/TestsAreWorking.cs
@@ -0,0 +1,11 @@
+namespace Tests
+{
+    public class TestsAreWorking
+    {
+        [Fact]
+        public void Test_TestsAreWorking_Pass()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ XiaomiSoftwareManager/Dialogs/CustomDialog.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+
+namespace XiaomiSoftwareManager.Dialogs
+{
+    public partial class CustomDialog : Window
+    {
+        private readonly bool canBeClosed;
+        public enum DialogType
+        {
+            OK = 0,
+            OKCancel = 1,
+            YesNo = 2,
+        }
+
+        public CustomDialog(string message, string title = "Custom Dialog", DialogType dialogType = DialogType.OK)
+        {
+            InitializeComponent();
+            this.Title = title;
+            MessageText.Text = message;
+
+            switch (dialogType)
+            {
+                case DialogType.OKCancel:
+                    FirstButton.Content = "OK";
+                    SecondButton.Content = "Cancel";
+                    SecondButton.Visibility = Visibility.Visible;
+                    break;
+
+                case DialogType.YesNo:
+                    FirstButton.Content = "Yes";
+                    SecondButton.Content = "No";
+                    SecondButton.Visibility = Visibility.Visible;
+                    break;
+
+                default:
+                    FirstButton.Content = "OK";
+                    SecondButton.Visibility = Visibility.Collapsed;
+                    break;
+            }
+        }
+
+        private void FirstButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (canBeClosed)
+            {
+                Close();
+                return;
+            }
+            this.Hide();
+        }
+
+        private void SecondButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (canBeClosed)
+            {
+                Close();
+                return;
+            }
+            this.Hide();
+        }
+    }
+}
